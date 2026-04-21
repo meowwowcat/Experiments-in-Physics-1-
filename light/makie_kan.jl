4861f721d4d6f6ba1144956bcb4a083c1917dfe9
meowwowcat <24cb062h@rikkyo.ac.jp>
2026-04-21 11:49:21 +0900

    data update

--- light/makie_kan.jl
+++ light/makie_kan.jl
@@ -1,8 +1,7 @@
-
-# 高速化されたコード
+# 高速化されたコード (Makie使用)
 using CSV
 using DataFrames
-using Plots: plot, plot!, savefig
+using CairoMakie
 using Printf
 
 # データ読み込み
@@ -15,11 +14,11 @@ l = df1[:, 1]
 direct = df2[:, 2]
 
 # グラフの初期化
-p = plot(xlabel="Wave lenght (nm)", ylabel="Transmittance ", legend=:topright)
-over = plot(xlabel="Wave lenght (nm)", ylabel="Transmittance",
-    label=false,legend=:topleft,
-    xlims=(500, 575),ylims=(0, 0.4)
-    )
+fig1 = Figure()
+ax1 = Axis(fig1[1,1], xlabel="Wavelength (nm)", ylabel="Transmittance", title="Interference Filter")
+fig2 = Figure()
+ax2 = Axis(fig2[1,1], xlabel="Wavelength (nm)", ylabel="Transmittance",
+    limits=(500, 575, 0, 0.4), title="Normalized Transmittance")
 
 # ベクトル化で高速化
 data_matrix = Matrix(df1[:, 2:15])  # 2列目から15列目を行列に
@@ -30,15 +29,18 @@ column_names = names(df1)[2:15]
 
 # プロットを一度に追加（ベクトル化）
 for (i, col) in enumerate(eachcol(data_matrix))
-    plot!(p, l, col, label=column_names[i])
+    lines!(ax1, l, col, label=column_names[i])
 end
 
 for (i, col) in enumerate(eachcol(over_matrix))
-    plot!(over, l, col, label=column_names[i] * "°")
+    lines!(ax2, l, col, label=column_names[i] * "°")
 end
 
-savefig(p, "light/透過率/figure/干渉フィルター.png")
-savefig(over, "light/透過率/figure/Transmittance_干渉フィルター.png")
+axislegend(ax1, position=:rt)
+axislegend(ax2, position=:lt)
+
+save("light/透過率/figure/干渉フィルター_makie.pdf", fig1)
+save("light/透過率/figure/Transmittance_干渉フィルター_makie.pdf", fig2)
 
 # 最大透過率の波長を調べる
 println("\n最大透過率の波長:")
@@ -58,10 +60,9 @@ results = DataFrame(
 # CSVに保存
 CSV.write("light/透過率/data/kan_最大透過率.csv", results)
 
-
 # 入射角と最大透過率の関係
-pz = plot(results.Column, results.Max_Transmittance,legend=:false,
-    xlabel="Angle of Incidence (θ)", ylabel="Maximum Transmittance")
-savefig(pz, "light/透過率/figure/波長_最大透過率.png")
-
-
+angles = [parse(Int, replace(col, "dig" => "")) for col in column_names]
+fig3 = Figure()
+ax3 = Axis(fig3[1,1], xlabel="Angle of Incidence (θ)", ylabel="Maximum Transmittance")
+lines!(ax3, angles, vec(max_vals))
+save("light/透過率/figure/波長_最大透過率_makie.pdf", fig3)
